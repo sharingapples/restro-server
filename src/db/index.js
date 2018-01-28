421413428ef0175e58a1455dc06b6ad10a7e4df6
Ranjan Shrestha <ranjan@sharingapples.com>
2018-01-28 16:05:08 +0545

    Limit number of records

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,9 @@ const FileSync = require('lowdb/adapters/FileAsync');
 const User = require('./User');
 const models = require('../models');
 
+const MAX_RECORDS = 10000; // The maximum number of records to keep in each model
+const LIMIT_RECORDS = 5000; // The number of records to keep once max records is reached
+
 const defaultValue = {
   users: [User.create('super', 'Super User', 'super', 'PImas123*')],
 };
@@ -17,6 +20,14 @@ module.exports = async function createDatabase() {
   const adapter = new FileSync('restro.db', {
     defaultValue,
     serialize: (data) => {
+      // Keep the maximum record count to a limit
+      Object.keys(models).forEach((model) => {
+        const m = data[model];
+        if (m && m.length > MAX_RECORDS) {
+          m.splice(0, m.length - LIMIT_RECORDS);
+        }
+      });
+
       const str = JSON.stringify(data);
       const checksum = md5(`${str}.AVOID_TAMPERING`);
       return `[${checksum},${str}]`;
