a6b87cf9743b96d5974cf39eead797aea8dc8bb5
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-19 11:43:02 +0545

    Update list of items every time the order is updated

--- src/db/Order.js
+++ src/db/Order.js
@@ -2,14 +2,19 @@ const CachedDataStructure = require('./CachedDataStructure');
 
 class Order extends CachedDataStructure {
   async update(object, id) {
-    const original = super.get(id);
+    const original = this.get(id);
     // If the object's status changes to 'Cancelled' or 'Completed'
     // then also remove the object from the cache
     const res = await super.update(object, id);
     if (object.status !== original.status && (object.status === 'Cancelled' || object.status === 'Completed')) {
       // Fire the delete listener as well
-      delete super.cache(id);
+      delete this.cache(id);
       super.fireListeners('delete', original);
+    } else {
+      // Update the list of items
+      const items = await this.db.OrderItems.all('orderId=?', id);
+      this.get(id).items = items;
+      this.fireListeners('update', this.get(id));
     }
 
     return res;
